12153e168f8b7facf4c43ac67f74a8fff17c8c8c
FerPadial <fernandopadial@gmail.com>
2024-10-25 21:33:57 -0300

    atualiz

--- exercicios/ex008/topo.js
+++ exercicios/ex008/topo.js
@@ -1,17 +1,21 @@
-const head=document.head
+
 const body=document.body
 
-const estilo="<link rel='stylesheet' href='topo.css'>"
-head.innerHTML+=estilo
-
+const estilotopo=
+    "display: flex;"+
+    "justify-content: space-between;"+
+    "align-items: center;"+
+    "background-color: rgba(211, 211, 244, 0.774);"
 const topo=document.createElement("div")
 topo.setAttribute("id","topo")
-topo.setAttribute("class","topo")
-
+topo.setAttribute("style",estilotopo)
 body.prepend(topo)  //append e appendchild anexa no final e prepend anexa no começo no topo
+
+const estilo_img_logo="width: 150px;"
+
 const logo=
     "<div id='logo' class='logo'>"+
-        "<img src='../imagens/olho.JPG' title='CFB Cursos'/>"+
+        "<img src='../imagens/olho.JPG' title='CFB Cursos' style='"+estilo_img_logo+"'/>"+
     "</div>"
 
 topo.innerHTML+=logo
